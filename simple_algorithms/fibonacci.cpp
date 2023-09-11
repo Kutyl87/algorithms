6548c79d487565137f9ffc7fda8eeeaa2b841298
pkutyl <01168894@pw.edu.pl>
2023-09-11 23:15:27 +0200

    Add example fibonacci algorithms

---
+++ simple_algorithms/fibonacci.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+int slow_fibonacci(int n)
+{
+
+    if(n<=1)
+    {
+        return n;
+    }
+    else
+    {
+        return slow_fibonacci(n-1) + slow_fibonacci(n-2);
+    }
+}
+int faster_fibonacci(int n)
+{
+    std::vector<int> fibo(2);
+    if(n<=1)
+    {
+        return n;
+    }
+    fibo.push_back(0);
+    fibo.push_back(1);
+    for(int i = n+1;i<n;i++)
+    {
+        fibo.push_back(fibo[0] + fibo[1]);
+        
+    }
+    return fibo[1];
+}
+
+std::vector<int> return_fibonacci_sequence(int n)
+{
+    std::vector<int> fibonacci_sequence(n+1);
+    for(int i; i<n+1;i++)
+    {
+        fibonacci_sequence[i] = slow_fibonacci(i);
+    }
+    return fibonacci_sequence;
+}
+
+void cout_fibonacci_sequence(std::vector<int> fibo_seq)
+{
+    std::cout<<"Whole fibonacci sequence" << " : ";
+    for(int elem: fibo_seq)
+    {
+            std::cout << std::to_string(elem) << " ";
+    }
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    int fibo= slow_fibonacci(n);
+    std::vector<int> fibo_seq = return_fibonacci_sequence(n);
+    std::cout << "The " + std::to_string(n) + "th " "element of fibonnaci sequence is "<<std::to_string(fibo) <<"\n";
+    int fibo_faster= faster_fibonacci(n);
+    std::cout << "The " + std::to_string(n) + "th " "element of fibonnaci sequence is "<<std::to_string(fibo) <<"\n";
+    cout_fibonacci_sequence(fibo_seq);
+}
